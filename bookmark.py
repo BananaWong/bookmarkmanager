c7048a8e2aa871eec30a9de03a0fc20ac1068436
BananaWong <bananawongh@gmail.com>
2023-12-18 20:54:14 +0800

    Update bookmark.py

--- bookmark.py
+++ bookmark.py
@@ -1,66 +1,117 @@
 import tkinter as tk
-from tkinter import filedialog, messagebox, ttk
+from tkinter import filedialog, messagebox, simpledialog
+from html.parser import HTMLParser
 import os
-from bs4 import BeautifulSoup
 
-def load_bookmarks():
-    """Load the bookmarks file and display them in the treeview."""
-    file_path = filedialog.askopenfilename(filetypes=[("HTML files", "*.html")])
-    if file_path:
-        with open(file_path, 'r', encoding='utf-8') as file:
-            bookmarks_data = file.read()
+class Bookmark:
+    def __init__(self, title, url=None, parent=None):
+        self.title = title
+        self.url = url
+        self.children = []
+        self.parent = parent
 
-        soup = BeautifulSoup(bookmarks_data, 'html.parser')
-        treeview.delete(*treeview.get_children())  # Clear existing entries
-        for bookmark in soup.find_all('a'):
-            title = bookmark.text
-            url = bookmark.get('href', '')
-            treeview.insert('', 'end', values=(title, url))
+    def add_child(self, bookmark):
+        bookmark.parent = self
+        self.children.append(bookmark)
 
-        entry_file_path.delete(0, tk.END)
-        entry_file_path.insert(0, file_path)
+    def is_folder(self):
+        return self.url is None
 
-def remove_selected():
-    """Remove the selected bookmarks."""
-    selected_items = treeview.selection()
-    for item in selected_items:
-        treeview.delete(item)
+    def __str__(self):
+        return self.title if self.is_folder() else f"{self.title} [{self.url}]"
 
-def save_bookmarks():
-    """Save the updated bookmarks to a new file."""
-    file_path = filedialog.asksaveasfilename(defaultextension=".html", filetypes=[("HTML files", "*.html")])
-    if file_path:
-        with open(file_path, 'w', encoding='utf-8') as file:
-            file.write('<!DOCTYPE NETSCAPE-Bookmark-file-1>\n')
-            file.write('<META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=UTF-8">\n')
-            file.write('<TITLE>Bookmarks</TITLE>\n')
-            file.write('<H1>Bookmarks</H1>\n')
-            file.write('<DL><p>\n')
-            for child in treeview.get_children():
-                title, url = treeview.item(child)['values']
-                file.write(f'    <DT><A HREF="{url}">{title}</A>\n')
-            file.write('</DL><p>')
-        messagebox.showinfo("Success", "Bookmarks saved successfully!")
+class BookmarkParser(HTMLParser):
+    def __init__(self):
+        super().__init__()
+        self.root = Bookmark("Root")
+        self.stack = [self.root]
 
-# Set up the main window
-window = tk.Tk()
-window.title("Bookmark Manager")
+    def handle_starttag(self, tag, attrs):
+        if tag == 'a':
+            href = dict(attrs).get('href', '')
+            self.stack[-1].add_child(Bookmark(self.last_data, href))
+        elif tag == 'h3':
+            self.last_data = ''
 
-# Create widgets
-btn_load = tk.Button(window, text="Load Bookmarks", command=load_bookmarks)
-btn_save = tk.Button(window, text="Save Bookmarks", command=save_bookmarks)
-btn_remove = tk.Button(window, text="Remove Selected", command=remove_selected)
-entry_file_path = tk.Entry(window, width=50)
-treeview = ttk.Treeview(window, columns=('Title', 'URL'), show='headings')
-treeview.heading('Title', text='Title')
-treeview.heading('URL', text='URL')
+    def handle_endtag(self, tag):
+        if tag == 'dl':
+            self.stack.pop()
+        elif tag == 'h3':
+            folder = Bookmark(self.last_data)
+            self.stack[-1].add_child(folder)
+            self.stack.append(folder)
 
-# Layout widgets
-btn_load.grid(row=0, column=0, padx=5, pady=5)
-btn_save.grid(row=1, column=0, padx=5, pady=5)
-btn_remove.grid(row=2, column=0, padx=5, pady=5)
-entry_file_path.grid(row=0, column=1, padx=5, pady=5)
-treeview.grid(row=3, column=0, columnspan=2, padx=5, pady=5)
+    def handle_data(self, data):
+        self.last_data = data
 
-# Run the application
-window.mainloop()
+    def parse_bookmarks(self, html):
+        self.feed(html)
+        return self.root
+
+class BookmarkApp(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title('Bookmark Manager')
+        self.geometry('600x400')
+        self.create_widgets()
+        self.parser = BookmarkParser()
+        self.current_folder = None
+
+    def create_widgets(self):
+        self.load_button = tk.Button(self, text='Load Bookmarks', command=self.load_bookmarks)
+        self.load_button.pack()
+
+        self.back_button = tk.Button(self, text='Back', command=self.go_back)
+        self.back_button.pack()
+
+        self.bookmark_listbox = tk.Listbox(self, width=50)
+        self.bookmark_listbox.pack()
+        self.bookmark_listbox.bind('<Double-1>', self.open_folder)
+
+        self.delete_button = tk.Button(self, text='Delete Selected', command=self.delete_selected)
+        self.delete_button.pack()
+
+        self.save_button = tk.Button(self, text='Save Bookmarks', command=self.save_bookmarks)
+        self.save_button.pack()
+
+    def load_bookmarks(self):
+        file_path = filedialog.askopenfilename()
+        if file_path:
+            with open(file_path, 'r', encoding='utf-8') as file:
+                html = file.read()
+                self.root = self.parser.parse_bookmarks(html)
+                self.current_folder = self.root
+                self.populate_listbox()
+
+    def populate_listbox(self):
+        self.bookmark_listbox.delete(0, tk.END)
+        for item in self.current_folder.children:
+            self.bookmark_listbox.insert(tk.END, item)
+
+    def open_folder(self, event):
+        selected_index = self.bookmark_listbox.curselection()[0]
+        selected_item = self.current_folder.children[selected_index]
+        if selected_item.is_folder():
+            self.current_folder = selected_item
+            self.populate_listbox()
+
+    def go_back(self):
+        if self.current_folder.parent:
+            self.current_folder = self.current_folder.parent
+            self.populate_listbox()
+
+    def delete_selected(self):
+        selected_indices = self.bookmark_listbox.curselection()
+        for i in selected_indices[::-1]:
+            del self.current_folder.children[i]
+        self.populate_listbox()
+
+    def save_bookmarks(self):
+        file_path = filedialog.asksaveasfilename(defaultextension='.html')
+        if file_path:
+            # TODO: Convert self.root to HTML and write to file
+            pass
+
+if __name__ == '__main__':
+    app = BookmarkApp()
+    app.mainloop()
